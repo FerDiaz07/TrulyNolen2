d1645b1f52ad985b5a9ced676c50a86c0c4c0492
kevin296 <65751286+kevin296@users.noreply.github.com>
2023-11-28 11:53:53 -0600

    add

--- Controladores/crud_clientes.js
+++ Controladores/crud_clientes.js
@@ -14,40 +14,67 @@ crud_cliente.leer = (req, res) => {
 
 crud_cliente.agregarC = (req, res) => {
 
-        const btn_insert = req.body.Insert;
-        const id = req.body.id;
-        const cliente = req.body.cliente;
-        const identificador = req.body.identificador;
-        const rtn = req.body.rtn;
-        const direccion = req.body.direccion;
-        const contacto = req.body.contacto;
-        const telefono = req.body.telefono;
+    const btn_insert = req.body.Insert;
+    const btn_actualizar =req.body.actualizar;
+    const btn_borrar =req.body.borrar;
+    const id = req.body.id;
+    const cliente = req.body.cliente;
+    const identificador = req.body.identificador;
+    const rtn = req.body.rtn;
+    const direccion = req.body.direccion;
+    const contacto = req.body.contacto;
+    const telefono = req.body.telefono;
 
-        if(btn_insert){
-            con.query('insert into Clientes set ?',{clienteid:id,razonsocial:cliente,identificador:identificador,rtn:rtn}, (error,results) =>{
+    if(btn_insert){
+        con.query('insert into Clientes set ?',{clienteid:id,razonsocial:cliente,identificador:identificador,rtn:rtn}, (error,results) =>{
 
-                con.query('insert into Sucursales set ?',{direccion:direccion,contacto:contacto,telefono:telefono,clienteid:id}, (error,results) =>{
-                    
-                    if (error) {
-                        console.log(error);
-                    }
-                })
-
+            con.query('insert into Sucursales set ?',{direccion:direccion,contacto:contacto,telefono:telefono,clienteid:id}, (error,results) =>{
+                
                 if (error) {
                     console.log(error);
-                }else{            
-                    res.redirect('/truly');
                 }
-            });
+            })
+
+            if (error) {
+                console.log(error);
+            }else{            
+                res.redirect('/truly');
+            }
+        });
+    }
+     if(btn_actualizar){
+        con.query('update Clientes set ? where clienteid = ?',[ {clienteid:id,razonsocial:cliente,identificador:identificador,rtn:rtn},id], (error,results) =>{
+            con.query('update Sucursales set ? where  clienteid = ? ',[ {clienteid:id,razonsocial:cliente,identificador:identificador,rtn:rtn},id], (error,results) =>{
+                
+                if (error) {
+                    console.log(error);
+                }
+            })
+
+            if (error) {
+                console.log(error);
+            }else{            
+                res.redirect('/truly');
+            }
+        });
 
 
+     }
+     if(btn_borrar){
+        con.query('delete from Clientes where clienteid = ?',[ id ], (error,results) =>{
 
-        }
-         
+
+            if (error) {
+                console.log(error);
+            }else{            
+                res.redirect('/truly');
+            }
+            
+        });
+
+
+     }
 };
-
-
-
 
 export {crud_cliente};
 
